f278577968c6a6fe8ce7e38beb62b5cfc5368872
pranavpatel <pranavpatel986@gmail.com>
2022-06-24 01:26:18 +0530

    second commit

--- src/main/java/com/simplilearn/LoginRegistrationSpringBootSecurityThymeleafApplication.java
+++ src/main/java/com/simplilearn/LoginRegistrationSpringBootSecurityThymeleafApplication.java
@@ -1,13 +1,20 @@
  package com.simplilearn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoginRegistrationSpringBootSecurityThymeleafApplication {
 
+	
+	public static Logger logger=LoggerFactory.getLogger(LoginRegistrationSpringBootSecurityThymeleafApplication.class);
+	
 	public static void main(String[] args) {
+		logger.info("Application started successfully");
 		SpringApplication.run(LoginRegistrationSpringBootSecurityThymeleafApplication.class, args);
+		logger.info("Application finished");
 	}
 
 }
